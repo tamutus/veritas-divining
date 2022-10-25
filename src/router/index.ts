4df0dcfc9fe4c43236c629ae850f494dfc53946a
Lavra Tamutus <lavratamutus@gmail.com>
2022-10-25 02:09:17 -0400

    site map generation, disclaimer, better transition

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,51 +8,67 @@ const routes = [
   {
     path: "/home",
     name: "Veritas Divining",
+    category: "Veritas Services",
     component: () => import("@/views/LandingView.vue"),
   },
   {
     path: "/services",
     name: "Services",
+    category: "Veritas Services",
     component: () => import("@/views/TarotReadingServices.vue"),
   },
   {
     path: "/terms-of-service",
     name: "Terms of Service",
+    category: "Veritas Services",
     component: () => import("@/views/TermsOfService.vue"),
+  },
+  {
+    path: "/disclaimer",
+    name: "Disclaimer",
+    category: "Veritas Services",
+    component: () => import("@/views/Disclaimer.vue"),
   },
   {
     path: "/divination",
     name: "Divination",
+    category: "Reference",
     component: () => import("@/views/DivinationSpecifics.vue"),
   },
   {
     path: "/good-question",
     name: "Good Questions",
+    category: "Reference",
     component: () => import("@/views/GoodQuestions.vue"),
   },
   {
-    path: "/schedule",
+    path: "/bookings",
     name: "Schedule a reading",
+    category: "Veritas Services",
     component: () => import("@/views/SchedulingView.vue"),
   },
   {
     path: "/tarot",
     name: "Tarot",
+    category: "Reference",
     component: () => import("@/views/TarotHub.vue"),
   },
   {
     path: "/testimonials",
     name: "Testimonials",
+    category: "Veritas Services",
     component: () => import("@/views/Testimonials.vue"),
   },
   {
     path: "/resources",
     name: "Resources",
+    category: "Reference",
     component: () => import("@/views/TarotResources.vue"),
   },
   {
     path: "/ethics",
-    name: "ethics",
+    name: "Ethics",
+    category: "Reference",
     component: () => import("@/views/CodeOfConduct.vue"),
   },
   //wildcard
@@ -67,22 +83,64 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
-    if (savedPosition) {
-      return { ...savedPosition, behavior: "smooth" };
-    } else {
-      return { top: 0, left: 0, behavior: "smooth" };
-    }
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (savedPosition) {
+          resolve({ ...savedPosition });
+        } else {
+          resolve({ top: 0, left: 0 });
+        }
+      }, 700);
+    });
   },
 });
 
-const siteMap = routes.map((route) => {
-  if (route.path && route.name)
-    return {
+interface cleanRoute {
+  path: string;
+  name: string;
+  category: string | undefined;
+}
+
+const cleanRoutes: cleanRoute[] = [];
+for (const route of routes) {
+  if (route.path && route.name && route.name !== "NotFound") {
+    cleanRoutes.push({
       path: route.path,
       name: route.name,
-    };
-});
+      category: route.category,
+    });
+  }
+}
 
-export { routes };
+const siteMap = cleanRoutes.reduce(
+  (categorized: { [key: string]: any[] }, route: cleanRoute) => {
+    if (typeof route?.category === "string") {
+      if (!Object.keys(categorized).includes(route.category)) {
+        return {
+          ...categorized,
+          [route.category]: [route],
+        };
+      } else {
+        if (Array.isArray(categorized[route.category])) {
+          categorized[route.category].push(route);
+        }
+        return categorized;
+      }
+    } else {
+      if (!Object.keys(categorized).includes("Uncategorized")) {
+        return {
+          ...categorized,
+          Uncategorized: [route],
+        };
+      } else {
+        categorized.Uncategorized.push(route);
+        return categorized;
+      }
+    }
+  },
+  {}
+);
+
+export { routes, siteMap };
 
 export default router;
---
+++ src/views/Disclaimer.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h1>DISCLAIMER</h1>
+
+    <h2>I do not do Readings on the following:</h2>
+    <Quote class="thin-list">
+      <ul>
+        <li>Third parties and/or others without their consent</li>
+        <li>Medical or Death-related Queries</li>
+        <li>Past/Future Lives</li>
+        <li>large Legal Matters</li>
+        <li>
+          "What will the outcome be?" or other Future-related/Fortune-teller
+          type Queries; instead, be proactive and ask how best you can make
+          things go well for yourself with your own power...
+        </li>
+      </ul>
+    </Quote>
+    <p>
+      Please note that you are getting advised by a layperson- my advice is not
+      a replacement for things best left to professionals, such as therapists
+      and legal counsellors.
+    </p>
+    <p>
+      I am not responsible for your actions that you take after/upon receiving
+      my advice; also, know that the future is not set in stone, it is up to you
+      to make it what you will, not anyone else (especially not me).
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Quote from "@/components/ui/Quote.vue";
+</script>
+
+<style scoped>
+.thin-list {
+  margin-left: 13vw;
+  margin-right: 13vw;
+  transition: margin 0.3s ease;
+}
+li {
+  font-size: 1.2em;
+  line-height: 2em;
+}
+p {
+  margin: 20px 15vw;
+  transition: margin 0.3s ease;
+}
+@media screen and (max-width: 550px) {
+  .thin-list {
+    margin-left: 3vw;
+    margin-right: 3vw;
+  }
+  p {
+    margin: 20px 8vw;
+  }
+}
+</style>
